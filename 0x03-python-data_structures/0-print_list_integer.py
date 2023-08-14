9f1eca4de9e5de10b15f753269506122896fdd73
cecilacecsoh <ceciloluoch95@gmail.com>
2023-08-14 11:02:10 -0700

    python-data_structures

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-def print_list_integer(_list=[]):
-    for j in _list:
-    print('{:d}'.format(j))
+def print_list_integer(my_list=[]):
+    for py in range(len(my_list)):
+        print("{:d}".format(my_list[py]))
