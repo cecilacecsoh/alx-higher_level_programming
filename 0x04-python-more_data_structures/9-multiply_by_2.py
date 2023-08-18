f69cea944a0bc20da7aa17ae76690c61c6accbfa
cecilacecsoh <ceciloluoch95@gmail.com>
2023-08-18 12:35:36 -0700

    python-more_data_structures

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/python3i
 
 def multiply_by_2(cec_dictionary):
-    return {key: value*2 for key, value in cec_dictonary.items()}
+    new_dict = cec_dictionary.copy()
+    list_keys = list(new_dict.keys())
+
+    for i in list_keys:
+        new_dict[i] *= 2
+
+    return (new_dict)
