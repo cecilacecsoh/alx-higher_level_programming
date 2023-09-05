0dc25ca8a823ec22da34d05c908fb0ad17274493
cecilacecsoh <ceciloluoch95@gmail.com>
2023-09-05 10:30:02 -0700

    python-more_classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,7 +4,7 @@
 
 
 class Rectangle:
-    """ class rectangle"""
+    """ The class rectangle"""
     def __init__(self, width=0, height=0):
         """ Instantiation with optional width and height"""
         self.width = width
