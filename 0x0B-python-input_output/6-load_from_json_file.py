ade197da728f2c846b0ceac3714d7a9571bb3207
cecilacecsoh <ceciloluoch95@gmail.com>
2023-09-12 11:20:15 -0700

    input_output

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -9,7 +9,7 @@ import json
 
 def load_from_json_file(filename):
     """creates an Object from a "JSON file" You don’t need to manage;
-    file permissions / exceptions and exceptions if the JSON 
+    file permissions / exceptions and exceptions if the JSON
     string doesn’t represent an object."""
     with open(filename, 'r', encoding='utf-8') as file:
         create_obj = json.load(file)
