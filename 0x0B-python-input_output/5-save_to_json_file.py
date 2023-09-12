156806e92ebaa950bcf10fc9c12387470cb66403
cecilacecsoh <ceciloluoch95@gmail.com>
2023-09-12 11:18:03 -0700

    input_output

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,7 +8,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """writes an Object to a text file, using a JSON representation and 
+    """writes an Object to a text file, using a JSON representation and
     You don’t need to manage file permission exceptions"""
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(my_obj, file)
---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+a script that adds all arguments to a Python list,
+and then save them to a file:
+"""
+from sys import argv
+    """ Uses function save_to_json_file from 5-save_to_json_file.py
+        Uses function load_from_json_file from 6-load_from_json_file.py
+        The list saved as a JSON representation in a file named add_item.json
+        If the file doesn’t exist, it should be created
+        No need to manage file permissions / exceptions"""
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+filename = 'add_item.json'
+my_list = []
+try:
+    my_list = load_from_json_file(filename)
+except Exception:
+    save_to_json_file(my_list, filename)
+
+arg_len = len(argv)
+
+if arg_len > 1:
+    for n in range(1, arg_len):
+        my_list.append(argv[n])
+
+    save_to_json_file(my_list, filename)
