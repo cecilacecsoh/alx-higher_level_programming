0896f93081efab3edb5237a949d5bc314960732e
cecilacecsoh <ceciloluoch95@gmail.com>
2023-09-13 11:40:08 -0700

    inheritance

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 A function that inserts a line of text to a file, after each line
 containing a specific string (see example):
@@ -17,6 +16,5 @@ def append_after(filename="", search_string="", new_string=""):
                 lines[n:n + 1] = [line[n], new_string]
                 n += 1
             n += 1
-
-    with open(filename, "w", encoding="utf-8") as f:
+        with open(filename, "w", encoding="utf-8") as f:
         f.writelines(lines)
