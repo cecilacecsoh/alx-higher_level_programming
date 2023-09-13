0b7ddcbd6a3ad85e1072e8b9ca11c680843a313a
cecilacecsoh <ceciloluoch95@gmail.com>
2023-09-13 11:25:40 -0700

    inheritance

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,7 +6,7 @@ A function that adds a new attribute to an object if it’s possible
 
 
 def add_attribute(obj, objname, value):
-    """
+    """ adds a new attribute to an object if it’s possible
     args:
         obj: a class object
         objname: an object name
